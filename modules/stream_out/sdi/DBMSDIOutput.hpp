ed5b74970e9a0d8c7048060bd7168cc4a5e8f26e
Francois Cartegnie <fcvlcdev@free.fr>
2016-10-18 20:08:06 +0200

    sout: add SDI stream output
    Decklink vout backport, so
    this is the currently the only support

---
+++ modules/stream_out/sdi/DBMSDIOutput.hpp
@@ -0,0 +1,69 @@
+#ifndef DBMSDIOUTPUT_HPP
+#define DBMSDIOUTPUT_HPP
+
+#include "SDIOutput.hpp"
+
+#include <vlc_es.h>
+
+#include <DeckLinkAPI.h>
+
+namespace sdi_sout
+{
+    class DBMSDIOutput : public SDIOutput
+    {
+        public:
+            DBMSDIOutput(sout_stream_t *);
+            ~DBMSDIOutput();
+            virtual AbstractStream *Add(const es_format_t *); /* reimpl */
+            virtual void Del(AbstractStream *); /* reimpl */
+            virtual int Open(); /* impl */
+            virtual int Process(); /* impl */
+
+        protected:
+            VideoDecodedStream *videoStream;
+            AudioDecodedStream *audioStream;
+            int ProcessVideo(picture_t *);
+            int ProcessAudio(block_t *);
+
+        private:
+            IDeckLink *p_card;
+            IDeckLinkOutput *p_output;
+
+            BMDTimeScale timescale;
+            BMDTimeValue frameduration;
+            /* XXX: workaround card clock drift */
+            vlc_tick_t offset;
+
+            struct
+            {
+                es_format_t configuredfmt;
+                bool tenbits;
+                int nosignal_delay;
+                picture_t *pic_nosignal;
+            } video;
+
+            struct
+            {
+                es_format_t configuredfmt;
+                int i_rate;
+            } audio;
+
+            struct
+            {
+                uint8_t afd, ar;
+                unsigned afd_line;
+            } ancillary;
+
+            bool b_running;
+            int ConfigureVideo(const video_format_t *);
+            int ConfigureAudio(const audio_format_t *);
+            int Start();
+            const char *ErrorToString(long i_code);
+            IDeckLinkDisplayMode * MatchDisplayMode(const video_format_t *,
+                                                    BMDDisplayMode = bmdDisplayModeNotSupported);
+            int doProcessVideo(picture_t *);
+            picture_t * CreateNoSignalPicture(const char*, const video_format_t *);
+    };
+}
+
+#endif // DBMSDIOUTPUT_HPP
---
+++ modules/stream_out/sdi/SDIOutput.hpp
@@ -0,0 +1,35 @@
+#ifndef SDIOUTPUT_HPP
+#define SDIOUTPUT_HPP
+
+#include "SDIStream.hpp"
+#include <vlc_common.h>
+
+namespace sdi_sout
+{
+    class SDIOutput
+    {
+        public:
+            SDIOutput(sout_stream_t *);
+            virtual ~SDIOutput();
+            virtual int Open() = 0;
+            virtual int Process() = 0;
+            virtual AbstractStream *Add(const es_format_t *);
+            virtual int   Send(AbstractStream *, block_t *);
+            virtual void  Del(AbstractStream *);
+            virtual int   Control(int, va_list);
+
+        protected:
+            sout_stream_t *p_stream;
+            PictureStreamOutputBuffer videoBuffer;
+            BlockStreamOutputBuffer audioBuffer;
+
+        private:
+            static void *SoutCallback_Add(sout_stream_t *, const es_format_t *);
+            static void  SoutCallback_Del(sout_stream_t *, void *);
+            static int   SoutCallback_Send(sout_stream_t *, void *, block_t*);
+            static int   SoutCallback_Control(sout_stream_t *, int, va_list);
+            static void  SoutCallback_Flush(sout_stream_t *, void *);
+    };
+}
+
+#endif
