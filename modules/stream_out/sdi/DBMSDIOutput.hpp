316083da782f03b28af75380188422755f66300e
Francois Cartegnie <fcvlcdev@free.fr>
2018-07-24 12:52:31 +0200

    queuecc

--- modules/stream_out/sdi/DBMSDIOutput.hpp
+++ modules/stream_out/sdi/DBMSDIOutput.hpp
@@ -22,7 +22,8 @@ namespace sdi_sout
         protected:
             VideoDecodedStream *videoStream;
             AudioDecodedStream *audioStream;
-            int ProcessVideo(picture_t *);
+            CaptionsStream *captionsStream;
+            int ProcessVideo(picture_t *, block_t *);
             int ProcessAudio(block_t *);
 
         private:
@@ -53,6 +54,7 @@ namespace sdi_sout
             {
                 uint8_t afd, ar;
                 unsigned afd_line;
+                unsigned captions_line;
             } ancillary;
 
             bool b_running;
@@ -62,7 +64,7 @@ namespace sdi_sout
             const char *ErrorToString(long i_code);
             IDeckLinkDisplayMode * MatchDisplayMode(const video_format_t *,
                                                     BMDDisplayMode = bmdDisplayModeNotSupported);
-            int doProcessVideo(picture_t *);
+            int doProcessVideo(picture_t *, block_t *);
             picture_t * CreateNoSignalPicture(const char*, const video_format_t *);
     };
 }
