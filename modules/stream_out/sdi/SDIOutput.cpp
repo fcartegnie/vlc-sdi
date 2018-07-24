23f2101ed98ccfdae9a5dbbe67b69696b5470e83
Francois Cartegnie <fcvlcdev@free.fr>
2018-07-24 12:52:31 +0200

    queuecc

--- modules/stream_out/sdi/SDIOutput.cpp
+++ modules/stream_out/sdi/SDIOutput.cpp
@@ -31,6 +31,8 @@ AbstractStream *SDIOutput::Add(const es_format_t *fmt)
         s = new VideoDecodedStream(VLC_OBJECT(p_stream));
     else if(fmt->i_cat == AUDIO_ES)
         s = new AudioDecodedStream(VLC_OBJECT(p_stream));
+    else if(fmt->i_cat == SPU_ES && fmt->i_codec == VLC_CODEC_CEA608)
+        s = new CaptionsStream(VLC_OBJECT(p_stream));
     else
         s = NULL;
 
